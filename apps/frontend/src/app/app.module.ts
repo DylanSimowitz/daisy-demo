fd679dc46483b9cca1762adbe238f25858526d46
Dylan Simowitz <git@dylan.simowitz.com>
2022-07-08 00:44:12 -0700

    feat: 🎸 add clickoutside directive
    Directive outputs an event when a click occurs outside the target
    element

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { OptionsComponent } from './components/autocomplete/options/options.component';
+import { ClickOutsideDirective } from './directives/clickoutside.directive';
 
 @NgModule({
   imports: [BrowserModule, HttpClientModule, CommonModule],
   declarations: [
     OptionsComponent,
+    ClickOutsideDirective,
   ],
   providers: [],
   bootstrap: [AppComponent],
